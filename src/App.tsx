c538ebaeabaf4cb292d12992c41bc711fc375dc2
Christophe Rosset <tophe@topheman.com>
2019-12-25 11:30:01 +0100

    feat(refs): add react refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import "./App.css";
 import Image from "./Image";
 
@@ -6,6 +6,13 @@ const people = ["Alice", "Bob", "John", "Liza"];
 
 const App: React.FC = () => {
   const [user, setUser] = useState<string | null>(null);
+  const primaryRef = createRef<HTMLImageElement>();
+  const peopleRefs = people.reduce<{
+    [key: string]: React.Ref<HTMLImageElement>;
+  }>((acc, user) => {
+    acc[user] = createRef<HTMLImageElement>();
+    return acc;
+  }, {});
   return (
     <div>
       <div>
@@ -14,12 +21,16 @@ const App: React.FC = () => {
         })}
       </div>
       <p>
-        <Image variant="large" onClick={() => setUser(null)} />
+        <Image variant="large" onClick={() => setUser(null)} ref={primaryRef} />
       </p>
       <ul style={{ textAlign: "center" }}>
-        {people.map(user => (
+        {people.map((user, index) => (
           <li style={{ listStyle: "none" }} key={user}>
-            <Image key={user} onClick={() => setUser(user)} />
+            <Image
+              key={user}
+              onClick={() => setUser(user)}
+              ref={peopleRefs[user]}
+            />
             <p style={{ textAlign: "center" }}>{user}</p>
           </li>
         ))}
