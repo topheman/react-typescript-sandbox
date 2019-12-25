d9be65965e4f555f5dafa981ff93a3351bd501ca
Christophe Rosset <tophe@topheman.com>
2019-12-25 16:25:54 +0100

    feat(*): dont rely on indexes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React, { useState, useEffect, useRef, createRef } from "react";
+
 import "./App.css";
 import Image from "./Image";
 
-const people = ["Alice", "Bob", "John", "Liza"];
+const users = ["Alice", "Bob", "John", "Liza"];
+const items = ["", ...users];
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<string | null>(null);
+  const [user, setUser] = useState<string>("");
   const [infos, setInfos] = useState<any>({});
 
   // dynamically create refs
   const refs = useRef(
-    [null, ...people].map(() => createRef<HTMLImageElement>())
+    items.reduce<{
+      [key: string]: React.RefObject<HTMLImageElement>;
+    }>((acc, user) => {
+      acc[user || ""] = createRef<HTMLImageElement>();
+      return acc;
+    }, {})
   );
 
   // process images size based on refs + scrollY/scrollX
   function mount() {
     console.log("refs", refs);
-    const images = [null, ...people].map((user, index) => ({
+    const images = items.map(user => ({
       name: user,
-      ...refs?.current?.[index]?.current?.getBoundingClientRect().toJSON()
+      ...refs?.current?.[user]?.current?.getBoundingClientRect().toJSON()
     }));
     console.log("refs", refs, "infos", images);
     setInfos({
@@ -51,17 +58,17 @@ const App: React.FC = () => {
       <p>
         <Image
           variant="large"
-          onClick={() => setUser(null)}
-          ref={refs.current[0]}
+          onClick={() => setUser("")}
+          ref={refs.current[""]}
         />
       </p>
       <ul style={{ textAlign: "center" }}>
-        {people.map((user, index) => (
+        {users.map(user => (
           <li style={{ listStyle: "none" }} key={user}>
             <Image
               key={user}
               onClick={() => setUser(user)}
-              ref={refs.current[1 + index]}
+              ref={refs.current[user]}
             />
             <p style={{ textAlign: "center" }}>{user}</p>
           </li>
