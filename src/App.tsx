d53893f6c0fc4c176ee32b591a03130467b1c199
Christophe Rosset <tophe@topheman.com>
2019-12-28 11:46:04 +0100

    feat(react-spring): basic useTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,46 @@
-import React, { useState, useEffect, useRef, createRef } from "react";
+import React, { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
 import "./App.css";
 import Image from "./Image";
 
-const users = ["Alice", "Bob", "John", "Liza"];
-const items = ["", ...users];
+const originalUsers = ["Alice", "Bob", "John", "Liza"];
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<string>("");
-  const [infos, setInfos] = useState<any>({});
+  const [users, setUsers] = useState(originalUsers);
 
-  // dynamically create refs
-  const refs = useRef(
-    items.reduce<{
-      [key: string]: React.RefObject<HTMLImageElement>;
-    }>((acc, user) => {
-      acc[user || ""] = createRef<HTMLImageElement>();
-      return acc;
-    }, {})
-  );
-
-  // process images size based on refs + scrollY/scrollX
-  function mount() {
-    console.log("refs", refs);
-    const images = items.map(user => ({
-      name: user,
-      ...refs?.current?.[user]?.current?.getBoundingClientRect().toJSON()
-    }));
-    console.log("refs", refs, "infos", images);
-    setInfos({
-      scrollX: window.scrollX,
-      scrollY: window.scrollY,
-      list: images
-    });
-  }
-
-  useEffect(mount, [user]);
-
-  // make sure to run mount on load, once every images are loaded
-  // to ensure correct computation
-  useEffect(() => {
-    window.addEventListener("load", mount);
-  }, []);
+  const transitions = useTransition(users, item => item, {
+    from: { transform: "translate(500px, 0px)", opacity: 0 },
+    enter: { transform: "translate(0px, 0px)", opacity: 1 },
+    leave: { transform: "translate(-500px, 0px)", opacity: 0 }
+  });
 
   return (
     <div>
-      <pre style={{ position: "fixed", fontSize: "0.76em" }}>
-        {JSON.stringify(
-          {
-            user,
-            ...infos
-          },
-          null,
-          "  "
-        )}
-      </pre>
       <p>
-        <Image
-          variant="large"
-          onClick={() => setUser("")}
-          ref={refs.current[""]}
-        />
+        <button
+          type="button"
+          onClick={() =>
+            setUsers(users => [`foo${new Date().toISOString()}`, ...users])
+          }
+        >
+          Add user
+        </button>
+        - Click on image to remove them
       </p>
-      <ul style={{ textAlign: "center" }}>
-        {users.map(user => (
-          <li style={{ listStyle: "none" }} key={user}>
-            <Image
-              key={user}
-              onClick={() => setUser(user)}
-              ref={refs.current[user]}
-            />
-            <p style={{ textAlign: "center" }}>{user}</p>
-          </li>
+      <ul style={{ textAlign: "center", paddingLeft: 0 }}>
+        {transitions.map(({ item, props, key }) => (
+          <animated.li
+            style={{ listStyle: "none", cursor: "pointer", ...props }}
+            key={key}
+            onClick={() => {
+              console.log("click", { key, item });
+              setUsers(users => users.filter(user => user !== item));
+            }}
+          >
+            <Image />
+            <p style={{ textAlign: "center" }}>{item}</p>
+          </animated.li>
         ))}
       </ul>
     </div>
